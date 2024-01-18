94c2cd354fd7498a21609b34a47f219ab7b21fc3
Craige Jonard <craigejonard@gmail.com>
2024-01-18 16:12:04 +0800

    uploading files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <input type="number" className="form_num" name="Phone_Number" placeholder="* Phone No" required ></input> 
+    </>
+  )
+}
+
+export default App
